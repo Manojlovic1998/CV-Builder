24c68904d0d8f6ff6c2d8f6e110d7edc424ab762
Nemanja Manojlovic <manojlovic911@hotmail.com>
2021-10-23 16:34:30 +0200

    Initialize semantic layout in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import './App.css';
+import InfoContextProvider from "./store/InfoContextProvider";
+import Header from "./components/UI/Header";
+import CvContent from "./components/section-containers/CvContent";
+import Main from "./components/UI/Main";
 
 function App() {
-  return (<h1>Initial</h1>);
+    return (
+        <InfoContextProvider>
+            <Header>
+
+            </Header>
+            <Main>
+                <CvContent/>
+            </Main>
+        </InfoContextProvider>
+    );
 }
 
 export default App;
